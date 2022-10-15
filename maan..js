1c8f4de16e7b934cd19865ae9112164030cfde40
cali hassan <112267536+abdiqaadirmahamuud@users.noreply.github.com>
2022-10-15 00:30:35 -0700

    DOM Html

---
+++ maan..js
@@ -0,0 +1,16 @@
+// DOM WORKING
+
+
+const h1=document.getElementsByTagName("h1") [0];
+
+console.log(" h1 contains   ", h1 . innerText);
+
+h1.innerText="abdiqaadir";
+h1.innerText="xamse mahamuud";
+
+console.log(" h1 contains   ", h1 . innerText);
+
+h1.classList.add('text-green-500' );
+h1.classList.add('text-green-500' );
+
+
